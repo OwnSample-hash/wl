5d776a7caeb7c57894df7ac35f64cf078a2e113e
OwnSample <ownsample@tutanota.com>
2025-04-19 16:26:49 +0200

    Removed orders

--- migrations/04_trigger_prevent_update_final_price.sql
+++
@@ -1,4 +0,0 @@
-CREATE OR REPLACE TRIGGER `prevent_update_final_price` BEFORE
-UPDATE ON store_orders FOR EACH ROW BEGIN SIGNAL SQLSTATE '45000'
-SET MESSAGE_TEXT = 'Final price cannot be updated';
-END;
